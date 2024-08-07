e1bc313ff8cedac11e29c89781b0ed42b7a8f9d8
Pratik Dhamecha <dhamechapratik5@gmail.com>
2024-08-07 10:26:30 +0530

    try

--- config/db.js
+++ config/db.js
@@ -5,7 +5,11 @@ const mongoose = require('mongoose');
 const connectDB = async () => {
     try {
         // await mongoose.connect(process.env.DATABASE_URI);
-        const conn = await mongoose.connect(process.env.DATABASE_URI);
+        const conn = await mongoose.connect(process.env.DATABASE_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
+
 
         console.log(`Connected to database: ${conn.connections[0].name}`);
     }
