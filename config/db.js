c8d25eb0d461927b3fdc040bcd824fd8197001d7
Pratik Dhamecha <dhamechapratik5@gmail.com>
2024-08-07 10:08:42 +0530

    first_try

--- config/db.js
+++ config/db.js
@@ -4,7 +4,13 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.DATABASE_URI);
+        // await mongoose.connect(process.env.DATABASE_URI);
+        const conn = await mongoose.connect(process.env.DATABASE_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
+
+        console.log(`Connected to database: ${conn.connections[0].name}`);
     }
     catch (err){
         console.log("MongoDB connection failed. Exiting now...")
