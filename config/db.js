a76cbc56ef8134989e3244e247095ed106954e1d
Pratik Dhamecha <dhamechapratik5@gmail.com>
2024-08-07 10:33:28 +0530

    db file changed

--- config/db.js
+++ config/db.js
@@ -1,21 +1,15 @@
-
-
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const connectDB = async () => {
-    try {
-        // await mongoose.connect(process.env.DATABASE_URI);
-        const conn = await mongoose.connect(process.env.DATABASE_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true
-        });
+  try {
+    // await mongoose.connect(process.env.DATABASE_URI);
+    const conn = await mongoose.connect(process.env.DATABASE_URI);
+    console.log("MongoDB connected: " + conn);
 
-
-        console.log(`Connected to database: ${conn.connections[0].name}`);
-    }
-    catch (err){
-        console.log("MongoDB connection failed. Exiting now...")
-        process.exit();
-    }
-}
+    console.log(`Connected to database: ${conn.connections[0].name}`);
+  } catch (err) {
+    console.log("MongoDB connection failed. Exiting now...");
+    process.exit();
+  }
+};
 module.exports = connectDB;
